957d115f7c8e2655f5a414e046706005bd6fb8f6
snoW3heX <52304158+snoW3heX@users.noreply.github.com>
2020-12-09 09:33:10 +0800

    1
    1

---
+++ WordGame_v2.0/lib/filehandler.h
@@ -0,0 +1,15 @@
+#ifndef _FILE_HANDLER_H_
+#define _FILE_HANDLER_H_
+#include "stdio.h"
+
+#define LINE_TEXT_MAX 100
+
+FILE *openFile(char *filepath, char *openmode);
+int writeStrFromFile(char *filepath, char *str);
+int readStrFromFile(char *filepath, char str[][LINE_TEXT_MAX]);
+void addStrFromFile(char *filepath, char *str);
+
+
+#endif
+
+
